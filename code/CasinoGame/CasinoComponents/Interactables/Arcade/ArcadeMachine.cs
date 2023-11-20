f159e60fc57b0ff1cd2207c505d7f0786358ab74
walker407 <hscott407@gmail.com>
2023-11-20 12:08:10 -0600

    Adding arcade machine interactable to interactables scene. Updating shader test scene again

---
+++ code/CasinoGame/CasinoComponents/Interactables/Arcade/ArcadeMachine.cs
@@ -0,0 +1,16 @@
+﻿using Sandbox;
+
+namespace Casino;
+
+public class ArcadeMachine : BaseComponent, IInteractable
+{
+	[Property] uint ArcadeCost { get; set; } = 20;
+	public void Interact(GameObject player)
+	{
+		if(player.GetComponent<PlayerMoney>() is PlayerMoney money) 
+		{
+			money.TakeMoney( ArcadeCost );
+		}
+		
+	}
+}

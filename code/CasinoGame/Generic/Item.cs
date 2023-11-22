280f4a7389894ecb31e2fcdfea57105210438d6b
trende2001 <68961063+trende2001@users.noreply.github.com>
2023-11-22 21:35:45 +0100

    Item resource

---
+++ code/CasinoGame/Generic/Item.cs
@@ -0,0 +1,23 @@
+﻿using Sandbox;
+
+namespace Casino;
+
+[GameResource("Generic Item", "item", "Base item objects with a description", Icon = "lunch_dining", IconBgColor = "#1396ad")]
+public class Item : GameResource
+{
+	[Property]
+	public string Name { get; set; }
+	
+	[Property]
+	public string Description { get; set; }
+	
+	[Property]
+	[ResourceType("png")]
+	public string Icon { get; set; }
+	
+	[Property]
+	public Model Worldmodel { get; set; }
+	
+	[Property]
+	public Model Viewmodel { get; set; }
+}

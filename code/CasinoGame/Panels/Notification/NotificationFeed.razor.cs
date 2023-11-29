032d877fda44119e10588e87dbf678316587e461
walker407 <hscott407@gmail.com>
2023-11-29 11:34:30 -0600

    Adding initial code for notification feed ui

---
+++ code/CasinoGame/Panels/Notification/NotificationFeed.razor.cs
@@ -0,0 +1,39 @@
+﻿using Sandbox;
+
+namespace Casino;
+
+public partial class NotificationFeed : PanelComponent
+{
+	public static NotificationFeed Instance;
+	[Property] public float FadeOutTime { get; set; } = 5.0f;
+
+	public record Notification( string message, int type, RealTimeSince timeSinceAdded );
+	public List<Notification> Notifications { get; set; } = new List<Notification>();
+
+	public NotificationFeed()
+	{
+		Instance = this;
+	}
+
+	public void PushNotification(string  message, int type )
+	{
+		Notifications.Insert(0, new Notification( message, type, 0.0f ) );
+
+		StateHasChanged();
+	}
+
+	public override void Update()
+	{
+		base.Update();
+
+		if ( IsProxy )
+			return;
+
+		if(Notifications.RemoveAll(x => x.timeSinceAdded > FadeOutTime) > 0)
+		{
+			StateHasChanged();
+		}
+	}
+
+	protected override int BuildHash() => System.HashCode.Combine( Notifications );
+}

f98cb148da8f911326bdb091fb44f14e4145033c
walker407 <hscott407@gmail.com>
2023-12-04 20:01:50 -0600

    Adding initial code for Clock component

---
+++ code/CasinoGame/CasinoComponents/Dev/Environment/Clock.cs
@@ -0,0 +1,44 @@
+﻿using Sandbox;
+
+namespace Casino;
+
+public partial class Clock : BaseComponent, INetworkSerializable
+{
+	private static double GameTime { get; set; } = 0;
+
+	public static Clock Instance { get; private set; }
+
+	protected override void OnAwake()
+	{
+		base.OnAwake();
+
+		Instance = new Clock();
+	}
+
+	protected override void OnFixedUpdate()
+	{
+		base.OnFixedUpdate();
+
+		if ( IsProxy ) return;
+
+		GameTime += 0.05f * Time.Delta;
+
+		PrintTime();
+	}
+
+	[Broadcast]
+	public void PrintTime()
+	{
+		Log.Info( $"Game Time: {GameTime}" );
+	}
+
+	public void Write( ref ByteStream stream )
+	{
+		stream.Write( GameTime );
+	}
+
+	public void Read( ByteStream stream )
+	{
+		GameTime = stream.Read<double>();
+	}
+}
